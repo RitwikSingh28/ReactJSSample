34ed8d6b0b95148c19f08c74b54cc2a74c0fbd82
RitwikSingh28 <itsritwik26@gmail.com>
2022-11-24 21:43:06 +0530

    make name-add-functionality

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import {v4 as uuidv4} from 'uuid';
+
+import './style.css';
 
 function App() {
+  const [person, setPerson] = useState({name: "", age: 0});
+  const [people, setPeople] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(person.name && person.age){
+      setPeople([...people, person]);
+      setPerson({name: "", age: 0});
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <main>
+        <form onSubmit={e => handleSubmit(e)}>
+          <div className="form-div">
+            <label htmlFor="userName">Add Name:</label>
+            <input 
+              type="text" 
+              name='userName' 
+              id='userName' 
+              value={person.name} 
+              onChange={(e) => setPerson({...person, name: e.target.value})}/>
+          </div>
+          <div className="form-div">
+            <label htmlFor="age">Add Age:</label>
+            <input 
+              type="text" 
+              name='age' 
+              id='age' 
+              value={person.age} 
+              onChange={(e) => setPerson({...person, age: e.target.value})}/>
+          </div>
+          <input type="submit" value="Submit" />
+        </form>
+      </main>
+        <section>
+          {
+            people.map((person) => {
+              return <div className="person" key={uuidv4()}>
+                <h1>{person.name}</h1>
+                <h2>{person.age}</h2>
+              </div>;
+            })
+          }
+        </section>
+    </>
   );
 }
 
